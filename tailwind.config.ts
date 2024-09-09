40d1c9748e7ce5e2c490dcffcd2676d685308004
Denis Ohrimenko <woxer77@gmail.com>
2024-09-09 15:07:11 +0300

    feat: Added shadcn components, fixed tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,6 +35,10 @@ const config: Config = {
   		}
   	},
   	colors: {
+      transparent,
+      white,
+      black,
+      inherit,
   		gray: {
   			'100': '#f9f9f9',
   			'200': '#EDEDED',
@@ -97,6 +101,28 @@ const config: Config = {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
